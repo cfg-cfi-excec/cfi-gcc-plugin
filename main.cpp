2c9ad0e4a61a04169c8c099c2d10987bcd4884a6
MTelesklav <e1252288@student.tuwien.ac.at>
2020-07-17 11:01:05 +0200

    reformat function

--- main.cpp
+++ main.cpp
@@ -24,30 +24,47 @@ static struct plugin_info cfi_plugin_info =
 { "1.0", "GCC Plugin for instrumenting code with CFI instructions" };
 
 int plugin_init(struct plugin_name_args *plugin_info,
-                struct plugin_gcc_version *version)
-{
-  if(!plugin_default_version_check(version, &gcc_version))
-  {
-    std::cerr << "This GCC plugin is for version " << GCCPLUGIN_VERSION_MAJOR
-              << "." << GCCPLUGIN_VERSION_MINOR << "\n";
-    return 1;
-  }
+                struct plugin_gcc_version *version) {
+	if(!plugin_default_version_check(version, &gcc_version)) {
+		std::cerr << "This GCC plugin is for version " << GCCPLUGIN_VERSION_MAJOR
+			<< "." << GCCPLUGIN_VERSION_MINOR << "\n";
+		return 1;
+	}
 
-  register_callback(plugin_info->base_name,
-                    /* event */ PLUGIN_INFO,
-                    /* callback */ NULL,
-                    /* user_data */
-                    &cfi_plugin_info);
+	GCC_PLUGIN *gcc_plugin = NULL;
 
-  // Register the phase right after cfg
-  struct register_pass_info pass_info;
+	for (int i = 0; i < plugin_info->argc; i++) {
+		if (std::strcmp(plugin_info->argv[i].key, "cfi_implementation") == 0) {
+			const char* implementation = plugin_info->argv[i].value;
 
-  pass_info.pass = new GCC_PLUGIN_HCFI(g, plugin_info->argv, plugin_info->argc);
-  pass_info.reference_pass_name = "*free_cfg";
-  pass_info.ref_pass_instance_number = 1;
-  pass_info.pos_op = PASS_POS_INSERT_AFTER;
+			if (std::strcmp(implementation, "HCFI") == 0) {
+						std::string implementation = plugin_info->argv[i].value;
+				gcc_plugin = new GCC_PLUGIN_HCFI(g, plugin_info->argv, plugin_info->argc);
+				break;
+			} else {
+				std::cerr << "Invalid CFI Implementation declared (" << implementation << ")\n";
+				return 1;
+			}
 
-  register_callback(plugin_info->base_name, PLUGIN_PASS_MANAGER_SETUP, NULL, &pass_info);
+			break;
+		}
+	}
 
-  return 0;
+	register_callback(plugin_info->base_name,
+					/* event */ PLUGIN_INFO,
+					/* callback */ NULL,
+					/* user_data */
+					&cfi_plugin_info);
+
+	// Register the phase right after cfg
+	struct register_pass_info pass_info;
+
+	pass_info.pass = gcc_plugin;
+	pass_info.reference_pass_name = "*free_cfg";
+	pass_info.ref_pass_instance_number = 1;
+	pass_info.pos_op = PASS_POS_INSERT_AFTER;
+
+	register_callback(plugin_info->base_name, PLUGIN_PASS_MANAGER_SETUP, NULL, &pass_info);
+
+	return 0;
 }
