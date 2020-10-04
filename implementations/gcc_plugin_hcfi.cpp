e666155369085847d04019fd98de49296cc5e1a1
MTelesklav <e1252288@student.tuwien.ac.at>
2020-10-04 11:36:23 +0200

    removed printf-exclusion from HCFI plugin

--- implementations/gcc_plugin_hcfi.cpp
+++ implementations/gcc_plugin_hcfi.cpp
@@ -8,13 +8,6 @@
   void GCC_PLUGIN_HCFI::onFunctionEntry(std::string file_name, std::string function_name, basic_block firstBlock, rtx_insn *firstInsn) {
     // Don't instrument function entry of _main with a CHECKLABEL
     if (strcmp(function_name.c_str(), "_main") != 0) {
-      // Disable CFI temporarily during printf
-      // TODO: replace CFI_DBG7 with some sort of CFI_DISABLE instruction
-      // TODO: Fix CFI in printf
-      if (function_name.compare("printf") == 0) {
-        generateAndEmitAsm("CFI_DBG7 t0", firstInsn, firstBlock, false);
-      }
-
       int label = getLabelForIndirectlyCalledFunction(function_name, file_name);
 
       // only instrument functions, which are known to be called indirectly
@@ -35,13 +28,6 @@
   void GCC_PLUGIN_HCFI::onFunctionReturn(std::string file_name, std::string function_name, basic_block lastBlock, rtx_insn *lastInsn) {
     // Don't instrument function return of _main
     if (function_name.compare("_main") != 0) {
-      // Enable CFI again after printf
-      // TODO: replace CFI_DBG6 with some sort of CFI_ENABLE instruction
-      // TODO: Fix CFI in printf
-      if (function_name.compare("printf") == 0) {
-        generateAndEmitAsm("CFI_DBG6 t0", lastInsn, lastBlock, false);
-      }
-
       generateAndEmitAsm("CHECKPC", lastInsn, lastBlock, false);
     } else {
       // disable CFI from here on
