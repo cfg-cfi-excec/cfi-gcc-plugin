bb2a87240e3258af5b1d487f4069bf32de54d932
MTelesklav <e1252288@student.tuwien.ac.at>
2020-10-04 11:53:33 +0200

    removed printf-exclusion from HAFIX plugin

--- implementations/gcc_plugin_hafix.cpp
+++ implementations/gcc_plugin_hafix.cpp
@@ -4,12 +4,7 @@
       : GCC_PLUGIN(ctxt, arguments, argcounter) {}
 
   void GCC_PLUGIN_HAFIX::onFunctionEntry(std::string file_name, std::string function_name, basic_block firstBlock, rtx_insn *firstInsn) {
-    if (strcmp(function_name.c_str(), "printf") == 0) {
-      // Disable CFI temporarily during printf
-      // TODO: replace CFI_DBG7 with some sort of CFI_DISABLE instruction
-      // TODO: Fix CFI in printf
-      generateAndEmitAsm("CFI_DBG7 t0", firstInsn, firstBlock, false);
-    } else if (strcmp(function_name.c_str(), "_main") == 0) {
+    if (strcmp(function_name.c_str(), "_main") == 0) {
       // enable CFI from here on
       //TODO: replace CFI_DBG6 with some sort of CFI_ENABLE instruction
       generateAndEmitAsm("CFI_DBG6 t0", firstInsn, firstBlock, false);
@@ -29,12 +24,7 @@
       generateAndEmitAsm("CFIDEL " + std::to_string(readLabelFromTmpFile()), lastInsn, lastBlock, false);
     }
 
-    if (function_name.compare("printf") == 0) {
-      // Enable CFI again after printf
-      // TODO: replace CFI_DBG6 with some sort of CFI_ENABLE instruction
-      // TODO: Fix CFI in printf
-      generateAndEmitAsm("CFI_DBG6 t0", lastInsn, lastBlock, false);
-    } else if (function_name.compare("_main") == 0) {
+    if (function_name.compare("_main") == 0) {
       // disable CFI from here on
       //TODO: replace CFI_DBG7 with some sort of CFI_DISABLE instruction
       generateAndEmitAsm("CFI_DBG7 t0", lastInsn, lastBlock, false);
@@ -48,9 +38,7 @@
     }
 
     // TODO: remove exclusion list here (tmp fix for soft fp lib functions)
-    if (function_name.compare("printf") != 0 && !isFunctionExcludedFromCFI(function_name)) {
-      // Don't generate CFIRET for printf
-      // TODO: Fix CFI in printf
+    if (!isFunctionExcludedFromCFI(function_name)) {
       generateAndEmitAsm("CFIRET " + std::to_string(readLabelFromTmpFile()), tmpInsn, block, false);
     }
   }
