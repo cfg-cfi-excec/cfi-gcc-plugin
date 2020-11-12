08abcfa396c1f7c6dc5396cb458cec9f05dc4ea0
MTelesklav <e1252288@student.tuwien.ac.at>
2020-11-12 18:45:04 +0100

    added HAFIX exception

--- implementations/gcc_plugin_hafix.cpp
+++ implementations/gcc_plugin_hafix.cpp
@@ -12,10 +12,12 @@
     } else if (strcmp(function_name.c_str(), "exit") == 0) {
       // reset CFI state because exit() breaks out of CFG
       generateAndEmitAsm(CFI_RESET, firstInsn, firstBlock, false);
+    } 
+    
+    if (!isExcludedFromBackwardEdgeCfi(function_name)) {
+      generateAndEmitAsm("CFIBR " + std::to_string(readLabelFromTmpFile()), firstInsn, firstBlock, false);
+      writeLabelToTmpFile(readLabelFromTmpFile()+1);
     }
-
-    generateAndEmitAsm("CFIBR " + std::to_string(readLabelFromTmpFile()), firstInsn, firstBlock, false);
-    writeLabelToTmpFile(readLabelFromTmpFile()+1);
   }
 
   void GCC_PLUGIN_HAFIX::onFunctionRecursionEntry(std::string file_name, std::string function_name, basic_block firstBlock, rtx_insn *firstInsn) {
@@ -25,7 +27,7 @@
 
   void GCC_PLUGIN_HAFIX::onFunctionReturn(std::string file_name, std::string function_name, basic_block lastBlock, rtx_insn *lastInsn) {
 
-    if (function_name.compare("__main") != 0) {
+    if (function_name.compare("__main") != 0 && !isExcludedFromBackwardEdgeCfi(function_name)) {
       generateAndEmitAsm("CFIDEL " + std::to_string(readLabelFromTmpFile()), lastInsn, lastBlock, false);
     }
 
@@ -56,12 +58,14 @@
   }
 
   void GCC_PLUGIN_HAFIX::onIndirectFunctionCall(std::string file_name, std::string function_name, int line_number, basic_block block, rtx_insn *insn) {
-    rtx_insn *tmpInsn = NEXT_INSN(insn);
-    while (NOTE_P(tmpInsn)) {
-      tmpInsn = NEXT_INSN(tmpInsn);
-    }
+    if (!isExcludedFromForwardEdgeCfi(function_name)) {
+      rtx_insn *tmpInsn = NEXT_INSN(insn);
+      while (NOTE_P(tmpInsn)) {
+        tmpInsn = NEXT_INSN(tmpInsn);
+      }
 
-    generateAndEmitAsm("CFIRET " + std::to_string(readLabelFromTmpFile()), tmpInsn, block, false);
+      generateAndEmitAsm("CFIRET " + std::to_string(readLabelFromTmpFile()), tmpInsn, block, false);
+    }
   }
 
   GCC_PLUGIN_HAFIX *GCC_PLUGIN_HAFIX::clone() {
