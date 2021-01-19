a9b1b2ddfe073551eba84cdf180fe04018c3066c
MTelesklav <e1252288@student.tuwien.ac.at>
2021-01-19 17:15:21 +0100

    removed custom register name list

--- gcc_plugin.cpp
+++ gcc_plugin.cpp
@@ -23,41 +23,7 @@ GCC_PLUGIN::GCC_PLUGIN(gcc::context *ctxt, struct plugin_argument *arguments, in
   }
 
   std::string GCC_PLUGIN::getRegisterNameForNumber(unsigned regno) {
-    switch (regno) {
-      case  0: return "zero";
-      case  1: return "ra";
-      case  2: return "sp";
-      case  3: return "gp";
-      case  4: return "tp";
-      case  5: return "t0";
-      case  6: return "t1";
-      case  7: return "t2";
-      case  8: return "fp";
-      case  9: return "s1";
-      case 10: return "a0";
-      case 11: return "a1";
-      case 12: return "a2";
-      case 13: return "a3";
-      case 14: return "a4";
-      case 15: return "a5";
-      case 16: return "a6";
-      case 17: return "a7";
-      case 18: return "s2";
-      case 19: return "s3";
-      case 20: return "s4";
-      case 21: return "s5";
-      case 22: return "s6";
-      case 23: return "s7";
-      case 24: return "s8";
-      case 25: return "s9";
-      case 26: return "s10";
-      case 27: return "s11";
-      case 28: return "t3";
-      case 29: return "t4";
-      case 30: return "t5";
-      case 31: return "t6";
-      default: return NULL;
-    };
+    return ((char const*[])REGISTER_NAMES)[regno];
   }
 
   unsigned int GCC_PLUGIN::execute(function * fun) {   
